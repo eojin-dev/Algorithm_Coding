259dae5418af8e5886913e85279c62269e89378f
eojin-dev <122774432+eojin-dev@users.noreply.github.com>
2025-06-26 02:46:54 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,14 @@
+s = input()
+alpha = [-1]*26
+#문자열에 등장하지 않은 알파벳의 인덱스(요소)는 처음 설정한 -1 값 그대로 유지
+
+for i in range(len(s)):
+    ch = s[i]
+    index = ord(ch) - ord('a')
+    #0~25 사이 숫자로 변환(a와의 차이로 a->0, b->1...)
+    if alpha[index] == -1:
+        alpha[index] = i
+    #여러번 등장 시 값이 변하지 않도록
+
+print(*alpha)
+#리스트를 공백으로 나눠 한 줄로 출력
