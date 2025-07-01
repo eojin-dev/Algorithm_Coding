d3bd32c4be5920954c8f023b29ee847bd3818fa8
eojin-dev <122774432+eojin-dev@users.noreply.github.com>
2025-07-02 02:11:55 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -5,8 +5,10 @@ alpha = {chr(i): i - ord('a') + 1 for i in range (ord('a'), ord('z') + 1)}
 mapping = [alpha[chr] for chr in s]
 
 h = 0
+mod = 1234567891
+
 for i in range(l):
-    k = mapping[i] * 31**i
-    h += k
+    h += mapping[i] * 31**i
+    h %= mod
 
 print(h)
