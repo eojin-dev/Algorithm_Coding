1dd6397a97591e70793f65b0978759d7711637e3
eojin-dev <122774432+eojin-dev@users.noreply.github.com>
2025-07-03 01:59:24 +0900

    Update 최대공약수와 최소공배수.py
    유클리드 호제법
    math 라이브러리 사용 안함

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -4,3 +4,18 @@ a, b = map(int, input().split())
 
 print(math.gcd(a, b))
 print(math.lcm(a, b))
+
+#유클리드 호제법
+
+# def gcd(a, b):
+#     while b!=0:
+#         a, b = b, a%b   #b가 0이 될 때까지 a % b를 반복
+#     return a
+    
+# def lcm(a, b):
+#     return a*b // gcd(a, b) #공식
+
+# a, b = map(int, input().split())
+
+# print(gcd(a, b))
+# print(lcm(a, b))
